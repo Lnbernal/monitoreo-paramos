3b7c4a26f0d38e63fe1d90b049aff17aa834a307
lnbernal <luis.nicolas.bernal@gmail.com>
2026-05-20 20:04:44 -0500

    Agregar configuración para Render

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Instalar ODBC Driver 17 para SQL Server en Linux
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev
+pip install -r requirements.txt
